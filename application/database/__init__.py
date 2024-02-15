b33c6b4b1993f3e4e671f1342cd583fd19ca3a4c
Jack <jackkevinperkins03@gmail.com>
2024-02-15 21:29:50 +0000

    added more to model to allow for kanban creation as first resource type

--- application/database/__init__.py
+++ application/database/__init__.py
@@ -6,13 +6,14 @@ db = SQLAlchemy()
 
 def create_db():
 
-    from application.model.model import User_Account, TestTable
+    from application.model.model import User_Account
     db.create_all()
 
     try:
         admin_account = User_Account(username ="T1Admin", password = generate_password_hash(os.getenv("Password"),  method = 'pbkdf2:sha256'), email ="admint1@yes.com",name = "Jack", level = 10 )
         db.session.add(admin_account)
         db.session.commit()
+
     except Exception as e:
     
-        print("failed to add T1 admin")
+        print("failed to add T1 admin, admin already exists")
--- application/model/model.py
+++ application/model/model.py
@@ -1,9 +1,10 @@
 from application.database import db
 
-class perm_Groups  (db.Model):
-    __tablename__ = "permgroups"
+class Perm_Org  (db.Model):
+    __tablename__ = "permorg"
     id = db.Column (db.Integer, primary_key = True)
-    group_name = db.Column (db.String(100), nullable = False, unique= True)
+    org_name = db.Column (db.String(100), nullable = False, unique= True)
+
 
 class User_Account(db.Model):
     __tablename__ = "useraccounts"
@@ -14,7 +15,7 @@ class User_Account(db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True)
     name = db.Column(db.String(255), nullable=True, default="")
     level = db.Column(db.Integer,nullable = True, default =0)
-    group_id= db.Column(db.Integer,db.ForeignKey("permgroups.id"), nullable = True)
+    group_id = db.Column(db.Integer,db.ForeignKey("permorg.id"), nullable = True)
     
 
     def _dict(self):
@@ -26,23 +27,30 @@ class User_Account(db.Model):
             "name":self.name,
             "level" : self.level
         }
- 
-class TestTable (db.Model):
-    __tablename__ = "TestTable"
-    
-    id = db.Column(db.Integer, primary_key = True)
-    test_content = db.Column(db.String(100),nullable= False)
-
-    
 
 class Group_Resources(db.Model):
     __tablename__ = "groupresources"
     id = db.Column (db.Integer, primary_key = True)
-    group_id = db.Column(db.Integer,db.ForeignKey("permgroups.id"))
+    group_id = db.Column(db.Integer,db.ForeignKey("permorg.id"))
     resource_group_name = db.Column(db.String(200), nullable=False)
 
 #individual
 class Ind_Resource(db.Model):
     __tablename__ = "indresource"
     id = db.Column (db.Integer, primary_key = True)
+    group_id = db.Column(db.Integer,db.ForeignKey("permgroups.id"))
     resource_type = db.Column (db.Integer, nullable = False)
+    resource_name = db.Column (db.String(100), nullable = False)
+
+#kanban
+class Resource_one(db.Model):
+    __tablename__  ="resourceone"
+    id = db.Column (db.Integer, primary_key = True)
+    Parent_id = db.Column(db.Integer,db.ForeignKey("indresource.id"))
+    Kanban_name = db.Column(db.String(100), nullable = False)
+
+class Kanban_task (db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    Parent_id = db.Column(db.Integer,db.ForeignKey("resourceone.id"))
+    order = db.Column(db.Integer, nullable = False)
+    content = db.Column (db.String(100), nullable = False)
